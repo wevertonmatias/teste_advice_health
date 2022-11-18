a12f0523c4f0e8512f30092db5df4a4b238da7a4
Weverton Matias <weverton-matias@hotmail.com>
2022-11-18 16:56:38 -0300

    Adicionando elementos para a base do html.

--- carford/settings.py
+++ carford/settings.py
@@ -122,8 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/static/'
+STATIC_ROOT = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 

312a5e4a8b97084aea171fd887aee1776ea4552b
Weverton Matias <weverton-matias@hotmail.com>
2022-11-18 17:39:23 -0300

    Registrando o app owners responsavel pela regra de clientes e seu model.

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Owners(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, verbose_name="Nome Completo")
+    cpf = models.CharField(max_length=11, null=False, blank=False, verbose_name="CPF")
+    telefone = models.CharField(max_length=255, null=False, blank=False, verbose_name="Telefone (*)")
+
+    def __str__(self):
+        return "{} - {}".format(self.nome, self.cpf)

cd382a7f7c40812a7c61470565fc907e61631de5
Weverton Matias <weverton-matias@hotmail.com>
2022-11-19 16:11:34 -0300

    Adicionando o modelo Car e adicionando dependências de modelo e cor de carro.

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class ColorCar(models.Model):
+    description = models.CharField(max_length=255, null=False, blank=False)
+
+
+class ModelsCar(models.Model):
+    description = models.CharField(max_length=255, null=False, blank=False)
+
+class Car(models.Model):
+    description = models.CharField(max_length=255, null=False, blank=False)
+    color = models.ForeignKey(ColorCar, on_delete=models.SET_NULL, null=True, blank=False)
+    model_car = models.ForeignKey(ModelsCar, on_delete=models.SET_NULL, null=True, blank=False)

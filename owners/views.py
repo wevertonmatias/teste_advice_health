f7b7e5841f5fb93c7cfde5e2b3dc00295d37bb3e
Weverton Matias <weverton-matias@hotmail.com>
2022-11-19 11:00:18 -0300

    Adicionando funcionalidade de adicionar Owners.

--- owners/views.py
+++ owners/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+
+from .models import Owner
 
 class Owners(TemplateView):
     template_name = 'list.html'
+
+
+class OwnersCreate(CreateView):
+    template_name = 'create.html'
+    model = Owner
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('owner_list')

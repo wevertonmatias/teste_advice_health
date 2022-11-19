5f1d96dfc0f43e724410d9482e01663785c3b0e2
Weverton Matias <weverton-matias@hotmail.com>
2022-11-19 16:19:52 -0300

    Adicionando funcionalidade de adicionar um carro.

--- cars/urls.py
+++ cars/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('create/', CarsCreate.as_view(), name='car_create'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Car
+
 
 # Create your views here.
+class CarsCreate(CreateView):
+    template_name = 'create.html'
+    model = Car
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('car_create')
+
